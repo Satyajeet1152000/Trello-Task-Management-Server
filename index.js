15c32deee13c5cfa83183136d36ad2cf48ccaecb
Satyajeet Singh <satyajeet1152000@gmail.com>
2024-07-30 20:35:34 +0530

    server setup

---
+++ index.js
@@ -0,0 +1,47 @@
+import express from "express";
+import mongoose from "mongoose";
+import bodyParser from "body-parser";
+
+import authRoutes from "./routes/auth.js";
+import taskRoutes from "./routes/task.js";
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+mongoose.set("strictQuery", false);
+
+// =================== Middleware ====================
+app.use(bodyParser.json());
+
+// ================= Routes =================
+app.use("/api/auth", authRoutes);
+app.use("/api/tasks", taskRoutes);
+
+// invalid routes error handling
+app.use((req, res) => {
+    // Invalid request
+    res.json({
+        error: {
+            message: "Invalid Request",
+            statusCode: 404,
+        },
+    });
+});
+
+// Database connection
+mongoose
+    .connect(process.env.DB_URL)
+    .then(() => {
+        console.log(process.env.DB_URL);
+        console.log("Mongoose Database connected successfully.");
+        app.listen(PORT, () => {
+            console.log(`Server is listinning on port : ${PORT}`);
+        });
+    })
+    .catch((err) => {
+        console.log(process.env.DB_URL);
+        console.log(`Mongoose Database connection error. : ${err}`);
+    });
